33ed45538b2578b6e260ce630793b0fa8424dcf8
heqifatiha <heqifatihaputri@gmail.com>
2018-10-12 14:44:56 +0700

    add module timeoutable devise to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :lockable, :confirmable
+         :recoverable, :rememberable, :validatable, :lockable, :confirmable, :timeoutable, timeout_in: 30.minutes
 
   validates :full_name, presence: :true
   validates :full_name, uniqueness: :true
