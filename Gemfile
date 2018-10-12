2d80ac291e98b935fc0d1b695b9b3f2d947393cb
heqifatiha <heqifatihaputri@gmail.com>
2018-10-12 14:17:43 +0700

    add module locked and confirmable devise

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'devise'
 gem 'cancancan'
 gem 'rolify'
+gem "letter_opener", :group => :development
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :lockable, :confirmable
+
+  validates :full_name, presence: :true
+  validates :full_name, uniqueness: :true
+  validates_uniqueness_of :email
 end
