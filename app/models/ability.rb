823ea4a7d65316e2a3f158a541e0753369249cc9
heqifatiha <heqifatihaputri@gmail.com>
2018-10-24 17:37:46 +0700

    change ability again

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,7 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     user ||= User.new # guest user (not logged in)
-    if user.role.name
-      when "Global Admin"
+    if user.role.name.eql? "Global Admin"
       can :manage, :all
     else
       can :read, :all
